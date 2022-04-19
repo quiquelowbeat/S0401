a9ea05927a0fdc60818979c815a4d73b7031f1cb
quiquelowbeat <kikeebcn@gmail.com>
2022-04-19 11:56:42 +0200

    Added Gradle project

---
+++ S04T01N02GognomsNom/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t01/n02/S04T01N02GognomsNomApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.cognoms.nom.s04.t01.n02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T01N02GognomsNomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T01N02GognomsNomApplication.class, args);
+	}
+
+}
