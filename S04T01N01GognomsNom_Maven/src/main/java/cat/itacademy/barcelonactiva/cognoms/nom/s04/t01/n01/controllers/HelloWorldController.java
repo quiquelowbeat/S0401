9f83c419a4c41a46cc0c0d60647d44a9a220b6ff
quiquelowbeat <kikeebcn@gmail.com>
2022-04-19 11:34:00 +0200

    Renamed directories

--- S04T01N01GognomsNom_Maven/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t01/n01/controllers/HelloWorldController.java
+++ S04T01N01GognomsNom_Maven/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t01/n01/controllers/HelloWorldController.java
@@ -1,4 +1,4 @@
-package cat.itacademy.barcelonactiva.controllers;
+package cat.itacademy.barcelonactiva.cognoms.nom.s04.t01.n01.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,11 +10,11 @@ public class HelloWorldController {
 
     @GetMapping("/HelloWorld")
     public String saluda(@RequestParam(value = "nom", required = false, defaultValue = "UNKNOWKN") String nom){
-        return "Hola " + nom + "Estàs executant un projecte Maven.";
+        return "Hola " + nom + ". Estàs executant un projecte Maven.";
     }
 
     @GetMapping({"/HelloWorld2", "/HelloWorld2/{nom}"})
     public String saluda2(@PathVariable(required = false) String nom){
-        return "Hola " + nom + "Estàs executant un projecte Maven.";
+        return "Hola " + nom + ". Estàs executant un projecte Maven.";
     }
 }
