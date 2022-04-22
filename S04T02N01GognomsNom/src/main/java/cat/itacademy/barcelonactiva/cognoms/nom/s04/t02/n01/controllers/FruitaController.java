0a08883fbd6559fdaf1831ae3dc3d9aa6320b3f6
quiquelowbeat <kikeebcn@gmail.com>
2022-04-22 09:36:16 +0200

    working on 4.2.1

---
+++ S04T02N01GognomsNom/src/main/java/cat/itacademy/barcelonactiva/cognoms/nom/s04/t02/n01/controllers/FruitaController.java
@@ -0,0 +1,67 @@
+package cat.itacademy.barcelonactiva.cognoms.nom.s04.t02.n01.controllers;
+
+import cat.itacademy.barcelonactiva.cognoms.nom.s04.t02.n01.model.domain.FruitaEntity;
+import cat.itacademy.barcelonactiva.cognoms.nom.s04.t02.n01.model.services.FruitaServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class FruitaController {
+
+    @Autowired
+    FruitaServiceImpl fruitaService;
+
+    @PostMapping("/add")
+    public ResponseEntity<HttpStatus> add(@RequestBody FruitaEntity fruita){
+        try{
+            fruitaService.addFruita(fruita);
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        } catch(Exception e){
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<HttpStatus> update(@RequestBody FruitaEntity fruita){
+        if(fruitaService.updateFruita(fruita)){
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<HttpStatus> delete(@PathVariable("id") int id){
+        try {
+            fruitaService.deleteFruitaById(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+    }
+
+    @GetMapping("/getOne/{id}")
+    public ResponseEntity<FruitaEntity> getOne(@PathVariable("id") int id){
+        if(fruitaService.getFruitaById(id) == null){
+            return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(fruitaService.getFruitaById(id), HttpStatus.OK);
+        }
+
+    }
+
+    @GetMapping("/getAll")
+    public ResponseEntity<List<FruitaEntity>> getAll(){
+        if(fruitaService.getAllFruita().isEmpty()){
+            return new ResponseEntity<>(null, HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(fruitaService.getAllFruita(), HttpStatus.OK);
+        }
+    }
+
+}
